85c82810d3820c1ec6f4a35455819cd7198d914f
ddiw <lee74785764@gmail.com>
2026-02-04 20:19:04 +0900

    [Feat] 카운터로 가이딩 구현, /is_registered 서브스크라이버 구현

---
+++ src/amatta/amatta/guide_to_info.py
@@ -0,0 +1,107 @@
+# robot 1 구동하는 코드: 사용자가 입력한 이동 경로를 따라 탐색
+import rclpy
+from rclpy.node import Node
+
+import time
+import math
+import itertools
+from turtlebot4_navigation.turtlebot4_navigator import TurtleBot4Directions, TurtleBot4Navigator
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
+from nav2_simple_commander.robot_navigator import TaskResult
+from std_msgs.msg import Int32MultiArray, Bool
+from airport_guide_interfaces.msg import DbInfo
+
+
+class GuideToInfo(Node):
+    def __init__(self):
+        super().__init__('guide_to_info')
+        self.navigator = TurtleBot4Navigator()
+        self.registered = False
+
+        self.publisher = self.create_publisher(
+            Bool,
+            '/search_mode',
+            10
+        )
+
+        self.subscription = self.create_subscription(
+            DbInfo,
+            '/is_registered',
+            self.db_callback,
+            10
+        )
+        
+        self.timer = self.create_timer(0.5, self.timer_callback)
+
+
+        # 4. 목표 지점(장소 DB) 정의
+        self.target_pose = [
+            # 0 입구 (Entrance)
+            {'name': 'Entrance',
+            'pose': self.create_pose(-3.26, 3.71, 0.9881, 0.1536)}
+        ]
+
+        ################ 테스트 완료 후 초기 위치만 남기기 ###############
+        # 1. 초기화 및 Docking 상태 확인
+        if not self.navigator.getDockedStatus():
+            self.navigator.info('Docking before initialising pose')
+            self.navigator.dock()
+
+        # 2. 초기 위치 설정
+        initial_pose = self.navigator.getPoseStamped([0.0, 3.0], TurtleBot4Directions.NORTH)
+        self.navigator.setInitialPose(initial_pose)
+
+        # 3. Nav2 활성화 대기 및 Undock
+        self.navigator.waitUntilNav2Active()
+        self.navigator.undock()
+
+    def timer_callback(self):
+        if not self.registered:
+            msg = Bool()
+            msg.data = True
+        
+        self.publisher.publish(msg)
+        # self.get_logger().info(f'Publishing search_mode: {msg.data}')
+
+    def db_callback(self, msg):
+        self.registered = msg.registered
+        if self.registered:
+            self.get_logger().info(f'Guide to Counter...')
+            self.navigator.startToPose(target_pose)
+
+
+        
+    def create_pose(self, x, y, z_orient, w_orient):
+        pose = PoseStamped()
+        pose.header.frame_id = 'map'
+        pose.header.stamp = self.navigator.get_clock().now().to_msg()
+        pose.pose.position.x = float(x)
+        pose.pose.position.y = float(y)
+        pose.pose.position.z = 0.0
+        pose.pose.orientation.x = 0.0
+        pose.pose.orientation.y = 0.0
+        pose.pose.orientation.z = float(z_orient)
+        pose.pose.orientation.w = float(w_orient)
+        return pose
+
+def main(args=None):
+    rclpy.init(args=args)
+    
+    # 클래스 인스턴스 생성 및 실행
+    guide = GuideToInfo()
+
+    try:
+        # [핵심 수정] 데이터가 들어올 때까지 Node를 Spin(대기) 시킵니다.
+        while rclpy.ok():
+            rclpy.spin(guide)
+            
+    except KeyboardInterrupt:
+        pass
+        
+    finally:
+        guide.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
--- src/amatta/setup.py
+++ src/amatta/setup.py
@@ -29,6 +29,7 @@ setup(
             'visited = amatta.visited_history_patrol:main',
             'lost = amatta.lost_spot_patrol:main',
             'test_pub = amatta.test_pub:main',
+            'guide = amatta.guide_to_info:main',
         ],
     },
 )
