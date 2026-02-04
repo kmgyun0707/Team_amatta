ca227e5c848fa831ab5ac35c696a019145f172bc
merong564 <kmj564@sookmyung.ac.kr>
2026-02-04 11:46:16 +0900

    [Feat] robot1 move_brute

---
+++ src/amatta/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'amatta'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rokey',
+    maintainer_email='kmj564@sookmyung.ac.kr',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'move_brute = amatta.move_test_brute:main'
+        ],
+    },
+)
